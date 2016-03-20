aecf50d8408082f61d499ade1f9352f787e0fe17
Romain Vigo Benia <romain.vigobenia@gmail.com>
2016-03-20 20:46:15 +0100

    Adds sample projects and categories to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Categories
+categories = Category.create!([
+  { name: 'Apiculture' },
+  { name: 'Aquaculture & Pêche' },
+  { name: 'Boissons alcoolisées' },
+  { name: 'Boissons non alcoolisées' },
+  { name: 'Elevage' },
+  { name: 'Epicerie salée' },
+  { name: 'Epicerie sucrée' },
+  { name: 'Horticulture' },
+  { name: 'Innovation' },
+  { name: 'Nature & Environnement' },
+  { name: 'Produits laitiers' },
+  { name: 'Viticulture' }
+])
+
+
+# Projects
+projects = Project.create!([
+  {
+    name: 'Lambic et bière de terroir !',
+    description: "Fermentation spontanée, malt local, recherches sur les houblons: des bières au service d'un lieu culturel...",
+    amount: 1100,
+    category: categories[2]
+  }, {
+    name: 'Un labo pour nos escargots',
+    description: 'Les Escargots de Lassouts ont besoin de votre aide, pour être transformés sur place et pour pouvoir élaborer de nouvelles recettes.',
+    amount: 5000,
+    category: categories[4]
+  }, {
+    name: 'Les coques de Cabourg',
+    description: "Contribuez à l'édition limitée d'un livre destiné à donner naissance à ce qui pourrait bien devenir LA spécialité gastronomique de Cabourg.",
+    amount: 3500,
+    category: categories[1]
+  }
+])
+
